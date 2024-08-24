07ad2b842e21baa7bc2e58d9620a516da851b96f
Veljko741 <118315103+Veljko741@users.noreply.github.com>
2024-08-24 23:15:44 +0200

    Add files via upload

---
+++ vezba.php
@@ -0,0 +1,22 @@
+<?php
+//Domaci zadatak
+$ime = "Veljko";
+$godine = 23;
+
+echo "Pozdrav, ja se zovem $ime , i imam $godine godine.";
+
+
+
+$number= 15;
+$number2= 12;
+$numberTotal = $number+$number2;
+
+echo "Rezultat:$numberTotal";
+
+
+
+
+
+$price = 500;
+$tax = 100;
+?>
